434686c5e656c3da08b4efd58fd1427f6dd13f9b
endorphin82 <endorphin82@gmail.com>
2019-03-26 04:05:51 +0200

    001-add_hello_world

---
+++ src/components/Hello.android.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {View, Text,  StyleSheet} from 'react-native';
+
+class Hello extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Android</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({})
+
+export default Hello;
